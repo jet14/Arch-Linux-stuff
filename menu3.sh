0564c60d5f30bc8275dca8f6c1a6c599a804e455
jet14 <47400777+jet14@users.noreply.github.com>
2019-03-04 20:52:24 +0000

    Add files via upload

---
+++ menu3.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+show_menu(){
+    NORMAL=`echo "\033[m"`
+    MENU=`echo "\033[36m"` #Blue
+    NUMBER=`echo "\033[33m"` #yellow
+    FGRED=`echo "\033[41m"`
+    RED_TEXT=`echo "\033[31m"`
+    ENTER_LINE=`echo "\033[33m"`
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 0)${MENU} Insert Menu Title for commands Here ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 4)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 5)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 6)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 7)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 8)${MENU}  ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 9)${MENU}  ${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Please enter a menu option or ${RED_TEXT}enter to exit. ${NORMAL}"
+    read -n1 opt 
+    
+}
+function option_picked() {
+    COLOR='\033[01;31m' # bold red
+    RESET='\033[00;00m' # normal white
+    MESSAGE=${@:-"${RESET}Error: No message passed"}
+    echo -e "${COLOR}${MESSAGE}${RESET}"
+}
+
+clear
+show_menu
+while [ opt != '' ]
+    do
+    if [[ $opt = "" ]]; then 
+            exit;
+    else
+        case $opt in
+        0) clear;
+           option_picked "Option 0 Picked";
+           #Insert your command here; 
+           show_menu;
+           ;;
+
+   
+        1) clear;
+           option_picked "Option 1 Picked";
+           ; 
+           show_menu;
+           ;;
+
+        2) clear;
+            option_picked "Option 2 Picked";
+            ;
+            show_menu;
+            ;;
+
+        3) clear;
+            option_picked "Option 3 Picked";
+            ;
+            show_menu;
+            ;;
+
+        4) clear;
+            option_picked "Option 4 Picked";
+            ;
+            show_menu;
+            ;;
+
+
+	5) clear;
+	    option_picked "Option 5 Picked";
+	    ;
+	    show_menu;
+	    ;;
+	
+	6) clear;
+            option_picked "Option 6 Picked";
+            ;
+            show_menu;
+            ;;
+
+        7) clear;
+            option_picked "Option 7 Picked";
+            ;
+            show_menu;
+            ;;
+
+	8) clear;
+            option_picked "Option 8 Picked";
+            ;
+            show_menu;
+            ;;
+        9) clear;
+            option_picked "Option 9 Picked";
+            ;
+            show_menu;
+            ;;
+
+
+        x)exit;
+        ;;
+
+        \n)exit;
+        ;;
+
+        *)clear;
+        option_picked "Pick an option from the menu";
+        show_menu;
+        ;;
+    esac
+fi
+done
